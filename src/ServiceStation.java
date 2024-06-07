bc3affe0fc269f52e278217200efae3a6d034b8c
chemguide <argentum198@gmail.com>
2024-06-08 00:31:35 +0500

    ready to go

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,21 +1,19 @@
 public class ServiceStation {
     public void check(Car car) {
-        System.out.println("Обслуживаем " + car.getModelName());
         this.motorComplect(car);
     }
 
     public void check(Bicycle bicycle) {
-        System.out.println("Обслуживаем " + bicycle.getModelName());
         this.updateTyre(bicycle);
     }
 
     public void check(Truck truck) {
-        System.out.println("Обслуживаем " + truck.getModelName());
         this.motorComplect(truck);
         this.checkTrailer();
     }
 
     public void updateTyre(Transport transport) {
+        System.out.println("Обслуживаем " + transport.getModelName());
         for (int i = 0; i < transport.getWeelsCount(); i++) {
             System.out.println("Меняем покрышку");
         }
